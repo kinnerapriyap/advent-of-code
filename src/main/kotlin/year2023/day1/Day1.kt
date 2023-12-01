fbba7f7b843cdedac8a7efed78bbe02ff914bc84
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2023-12-01 06:36:01 +0100

    Add day 1 solution

--- src/main/kotlin/year2023/day1/Day1.kt
+++ src/main/kotlin/year2023/day1/Day1.kt
@@ -4,16 +4,37 @@ import utils.setup.Day
 
 fun main() = Day1().printDay()
 
-class Day1 : Day(dayNumber = 1, year = 2023) {
+class Day1 : Day(dayNumber = 1, year = 2023, useSampleInput = true) {
+    override fun partOne(): Any =
+        inputList.sumOf { str ->
+            str.toList().run {
+                (firstOrNull { it.isDigit() }?.digitToInt() ?: 0) * 10 +
+                        (lastOrNull { it.isDigit() }?.digitToInt() ?: 0)
+            }
+        }
 
-    private var ans: List<String> = listOf()
-    init {
-        ans = listOf()
+    override fun partTwo(): Any =
+        inputList.sumOf { str ->
+            extractDigits(str).toList().run {
+                first().digitToInt() * 10 + last().digitToInt()
+            }
+        }
+
+    private fun extractDigits(input: String): String = buildString {
+        val digitWords = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        var current = ""
+        input.forEach { char ->
+            if (char.isLetter()) {
+                current += char
+                digitWords.find { it in current }?.let {
+                    append(digitWords.indexOf(it) + 1)
+                    current = it.last().toString()
+                }
+            } else if (char.isDigit()) {
+                append(char)
+                current = ""
+            }
+        }
     }
 
-    val input = inputList
-
-    override fun partOne(): Any {
-        TODO("Not yet implemented")
-    }
 }
