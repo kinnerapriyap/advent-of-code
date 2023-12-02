bcd2efd8a73d703e833915777dc49004cc0d78fa
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2023-12-02 06:32:05 +0100

    Add for day 3

---
+++ src/main/kotlin/year2023/solutions/Day3.kt
@@ -0,0 +1,12 @@
+package year2023.solutions
+
+import utils.setup.Day
+
+fun main() = Day3().printDay()
+
+class Day3 : Day(dayNumber = 3, year = 2023, useSampleInput = true) {
+    private val input = inputList
+    override fun partOne(): Any {
+        return input
+    }
+}
