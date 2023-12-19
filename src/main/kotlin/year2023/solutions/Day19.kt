6d8802e5b251203ac2b5943e21f3ab291ff09bfd
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2023-12-19 07:36:02 +0100

    Almost there for day 19 2023 part 2

--- src/main/kotlin/year2023/solutions/Day19.kt
+++ src/main/kotlin/year2023/solutions/Day19.kt
@@ -4,7 +4,7 @@ import utils.setup.Day
 
 fun main() = Day19().printDay()
 
-class Day19 : Day(dayNumber = 19, year = 2023, useSampleInput = false) {
+class Day19 : Day(dayNumber = 19, year = 2023, useSampleInput = true) {
     data class Instruction(
         val condition: String?,
         val workflow: String
@@ -14,6 +14,18 @@ class Day19 : Day(dayNumber = 19, year = 2023, useSampleInput = false) {
             return if (condition.contains(">")) map[f[0]]!! > num.toInt()
             else map[f[0]]!! < num.toInt()
         }
+    }
+
+    private fun HashMap<Char, IntRange>.changeForCorrectCondition(instruction: Instruction) {
+        val (f, num) = instruction.condition?.split(">", "<") ?: return
+        if (instruction.condition.contains(">")) this[f[0]] = IntRange(num.toInt() + 1, this[f[0]]!!.last)
+        else this[f[0]] = IntRange(this[f[0]]!!.first, num.toInt() - 1)
+    }
+
+    private fun HashMap<Char, IntRange>.changeForWrongCondition(instruction: Instruction) {
+        val (f, num) = instruction.condition?.split(">", "<") ?: return
+        if (instruction.condition.contains(">")) this[f[0]] = IntRange(this[f[0]]!!.first, num.toInt())
+        else this[f[0]] = IntRange(num.toInt(), this[f[0]]!!.last)
     }
 
     private val input = inputString.split("\n\n")
@@ -67,7 +79,50 @@ class Day19 : Day(dayNumber = 19, year = 2023, useSampleInput = false) {
     }
 
     override fun partTwo(): Any {
+        val ans = mutableListOf<HashMap<Char, IntRange>>()
+        workflows
+            .filter { (_, ins) -> ins.any { it.workflow == "A" } }
+            .forEach { (name, ins) ->
+                val indexes = ins.mapIndexed { index, instruction -> index to instruction }
+                    .filter { it.second.workflow == "A" }
+                indexes.forEach { (ind, i) ->
+                    ans += solve(name, ind)
+                }
+            }
 
-        return 0
+        return ans.joinToString("\n")
+    }
+
+    private fun solve(workflowName: String = "rfg", indexA: Int = 2): List<HashMap<Char, IntRange>> {
+        val end = "in"
+        val answers = mutableListOf<HashMap<Char, IntRange>>()
+        fun recursive(currName: String, indexOfCurr: Int, currHashMap: HashMap<Char, IntRange>) {
+            val workflowInstructions = workflows[currName]!!
+            val iBefore = workflowInstructions.subList(0, indexOfCurr)
+            currHashMap.changeForCorrectCondition(workflowInstructions[indexOfCurr])
+            iBefore.forEach { currHashMap.changeForWrongCondition(it) }
+            if (currName == end) {
+                answers.add(currHashMap)
+                return
+            }
+            workflows
+                .filter { it.key != currName && it.value.any { it.workflow == currName } }
+                .forEach { (hasCurrName, hasCurrInstructions) ->
+                    val indexes = hasCurrInstructions.mapIndexed { index, instruction -> index to instruction }
+                        .filter { it.second.workflow == currName }
+                    indexes.forEach { (indexCurr, _) ->
+                        recursive(hasCurrName, indexCurr, currHashMap)
+                    }
+                }
+        }
+
+        val forA = hashMapOf(
+            'x' to IntRange(1, 4000),
+            'm' to IntRange(1, 4000),
+            'a' to IntRange(1, 4000),
+            's' to IntRange(1, 4000),
+        )
+        recursive(workflowName, indexA, forA)
+        return answers
     }
 }
