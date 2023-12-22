d7c60d6af13ea56d2b41cb3b81521dba55f5939a
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2023-12-22 05:39:58 +0100

    Update base

--- src/main/kotlin/utils/setup/Day.kt
+++ src/main/kotlin/utils/setup/Day.kt
@@ -15,7 +15,7 @@ abstract class Day(
     }
 
     abstract fun partOne(): Any
-    open fun partTwo(): Any = ""
+    open fun partTwo(): Any = 0
 
     fun printDay() {
         val header = "Day $dayNumber, $year"
@@ -25,29 +25,5 @@ abstract class Day(
         println("Part 1: \n${partOne()}")
         println("Part 2: \n${partTwo()}")
         println(footer)
-    }
-
-    fun getNumbersAndPositions(): List<Pair<Int, List<Point>>> {
-        val colMax = inputList[0].length
-        val numbers = mutableListOf<Pair<Int, List<Point>>>()
-        var current = 0
-        var start: Point? = null
-        inputList
-            .mapIndexed { row, s ->
-                s.forEachIndexed { col, char ->
-                    if (char.isDigit()) {
-                        current = if (current == 0) {
-                            start = Point(row, col)
-                            char.digitToInt()
-                        } else current * 10 + char.digitToInt()
-                    } else if (current != 0) {
-                        val c = if (col == 0 && start!!.col != 0) colMax else col
-                        val r = if (col == 0 && start!!.col != 0) row - 1 else row
-                        numbers.add(current to (start!!.col until c).map { Point(r, it) })
-                        current = 0
-                    }
-                }
-            }
-        return numbers
     }
 }
